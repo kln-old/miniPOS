944601c2108f9c5c8fe743b22f6ce357e4f61849
kln <nitheeshkl@gmail.com>
2014-04-18 20:36:22 +0530

    added printer support & receipt formatting
    requires xhtml2pdf and pycups support

--- mpos/mpos_print.py
+++ mpos/mpos_print.py
@@ -10,6 +10,8 @@
 import wx.html
 from wx.html import HtmlEasyPrinting
 import sys
+import cups
+from xhtml2pdf import pisa
 
 class Printer(HtmlEasyPrinting):
     def __init__(self):
@@ -26,4 +28,30 @@ class Printer(HtmlEasyPrinting):
         except:
             pass
         
+class PrinterCups():
+	def __init__(self):
+		#pisa.__init_(self)
+		print "initializing pisa in printer..."
 
+	def PrintText(self, text, title):
+		#tmp file for printing
+		filename = "/tmp/print.pdf"
+
+		pdf = pisa.CreatePDF(text, file(filename, "w"))
+                if not pdf.err:
+                        # close pdf file else we can't read later
+                        pdf.dest.close()
+
+                        # print the file using cups
+                        conn = cups.Connection()
+                        # get a list of all printers
+                        printers = conn.getPrinters()
+                        for printer in printers:
+                                # print name of printers to stdout
+                                print printer, printers[printer]["device-uri"]
+                        
+                        #get first printer from the printer list
+                        printer_name = printers.keys()[0]
+                        conn.printFile(printer_name, filename, "python_status_print",{})
+                else:
+                        print "unable to create pdf file"
